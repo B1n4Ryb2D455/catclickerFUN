a96c470f7d0b4a4558d31579d337f8dbe80aeb43
Sara Blevins <dvlnthebludrs@gmail.com>
2018-07-15 15:10:50 -0400

    Fix menu and clean code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
+
 let clicks = 0;
 let clickCounter = document.querySelector(".clicks");
-let paulCounter = document.querySelector(".paul");
 
 var btn = document.querySelector('.featured-image');
 btn.onclick = function clickCount() {
@@ -8,23 +8,14 @@ btn.onclick = function clickCount() {
   clickCounter.innerHTML = clicks;
   console.log(clicks);
 }
-
-var btn = document.querySelector('.paul-image');
-btn.onclick = function clickCount() {
-  clicks++;
-  paulCounter.innerHTML = clicks;
-  console.log(clicks);
-}
 class Cat {
   constructor(name) {
     this.name = name;
   }
 }
+
 let catOne = new Cat("Goth Kitty");
 let catTwo = new Cat("Paul Kitty");
-
-let gothKitty = document.querySelector('.gK');
-gothKitty.innerHTML = `You have annoyed ${catOne.name}, congratulations...`;
-
-let paulKitty = document.querySelector('.pK');
-paulKitty.innerHTML = `You have annoyed ${catTwo.name}, congratulations...`;
+let catThree = new Cat("Sime Kitty");
+let catFour = new Cat("Toodles Kitty");
+let catFive = new Cat("Farva Kitty");
