2daf596e7de05e18379a16bc45461cc40d6469a2
Sara Blevins <dvlnthebludrs@gmail.com>
2018-07-15 15:25:30 -0400

    Hide cats by default

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let clicks = 0;
 let clickCounter = document.querySelector(".clicks");
 
-var btn = document.querySelector('.featured-image');
+var btn = document.querySelector('.catPic');
 btn.onclick = function clickCount() {
   clicks++;
   clickCounter.innerHTML = clicks;
@@ -13,9 +13,9 @@ class Cat {
     this.name = name;
   }
 }
-
 let catOne = new Cat("Goth Kitty");
 let catTwo = new Cat("Paul Kitty");
 let catThree = new Cat("Sime Kitty");
 let catFour = new Cat("Toodles Kitty");
 let catFive = new Cat("Farva Kitty");
+
