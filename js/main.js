e2f00e604e09a6d50389949730da3fbeb222be90
Sara Blevins <dvlnthebludrs@gmail.com>
2018-07-15 13:03:41 -0400

    Add counters to both cats

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-class Cat {
-  constructor(name) {
-    this.name = name;
-  }
-}
-let catOne = new Cat("Goth Kitty");
-let catTwo = new Cat("Paul Kitty");
-
 let clicks = 0;
-let clickCounter = document.querySelector('.clicks');
+let clickCounter = document.querySelector(".clicks");
+let paulCounter = document.querySelector(".paul");
 
 var btn = document.querySelector('.featured-image');
 btn.onclick = function clickCount() {
@@ -16,8 +9,25 @@ btn.onclick = function clickCount() {
   console.log(clicks);
 }
 
+var btn = document.querySelector('.paul-image');
+btn.onclick = function clickCount() {
+  clicks++;
+  paulCounter.innerHTML = clicks;
+  console.log(clicks);
+}
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+}
+let catOne = new Cat("Goth Kitty");
+let catTwo = new Cat("Paul Kitty");
+
 let gothKitty = document.querySelector('.gK');
-gothKitty.innerHTML = `You have annoyed ${catOne.name} `;
+gothKitty.innerHTML = `You have annoyed ${catOne.name}, congratulations...`;
 
 let paulKitty = document.querySelector('.pK');
-paulKitty.innerHTML = `You have annoyed ${catTwo.name} `;
+paulKitty.innerHTML = `You have annoyed ${catTwo.name}, congratulations...`;
+
+
