af04dd1e3d2ec70e9de0d729b3f318e2156ee76e
Sara Blevins <dvlnthebludrs@gmail.com>
2018-07-15 21:22:32 -0400

    Add cat array

--- js/main.js
+++ js/main.js
@@ -1,31 +1,66 @@
 
+let model = {
+  currentCat: null,
+  cats: [
+    {
+      clickCount: 0,
+      name: 'Goth Kitty',
+      imgSrc: '../img/blackcat.jpg',
+      imgAttribution: "https://pixbay.com"
+    }
+    {
+      clickCount: 0;
+      name: 'Paul Kitty',
+      imgSrc: '../img/paulcat.jpg',
+      imgAttribution: "https://pixbay.com"
+    }
+    {
+      clickCount: 0;
+      name: 'Sime Kitty';
+      imgSrc: '../img/simecat.jpg',
+      imgAttribution: "https://pixbay.com"
+    }
+    {
+      clickCount: 0;
+      name: 'Tootles Kitty',
+      imgSrc: '../img/toodlescat.jpg',
+      imgAttribution: "https://pixbay.com"
+    }
+    {
+      clickCount: 0;
+      name: 'Farva Kitty',
+      imgSrc: '../img/farvacat.jpg',
+      imgAttribution: "https://pixbay.com"
+    }
+  ]
+};
 
-class Cat {
+// class Cat {
 
-  constructor(name, clicks) {
-    this.name = name;
-    this.clicks = clicks;
-  }
+//   constructor(name, clicks) {
+//     this.name = name;
+//     this.clicks = clicks;
+//   }
 
-}
+// }
 
-let clicks = 0;
-let clickCounter = document.querySelector(".clicks");
+// let clicks = 0;
+// let clickCounter = document.querySelector(".clicks");
 
-let btn = document.querySelector('img');
-btn.onclick = function clickCount() {
-  clicks++;
-  clickCounter.innerHTML = clicks;
-  console.log(clicks);
-}
+// let btn = document.querySelector('img');
+// btn.onclick = function clickCount() {
+//   clicks++;
+//   clickCounter.innerHTML = clicks;
+//   console.log(clicks);
+// }
 
-let theCatPic = document.querySelectorAll('.catPic');
+// let theCatPic = document.querySelectorAll('.catPic');
 
-let catOne = new Cat("Goth Kitty",);
-let catTwo = new Cat("Paul Kitty");
-let catThree = new Cat("Sime Kitty",);
-let catFour = new Cat("Toodles Kitty");
-let catFive = new Cat("Farva Kitty");
+// let catOne = new Cat("Goth Kitty",);
+// let catTwo = new Cat("Paul Kitty");
+// let catThree = new Cat("Sime Kitty",);
+// let catFour = new Cat("Toodles Kitty");
+// let catFive = new Cat("Farva Kitty");
 
 
 
