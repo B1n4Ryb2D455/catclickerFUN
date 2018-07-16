cdf334ac3736eebec18d82fb074a8f9431f32839
Sara Blevins <dvlnthebludrs@gmail.com>
2018-07-15 21:14:51 -0400

    Update HTML per instructions

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
 
+
+class Cat {
+
+  constructor(name, clicks) {
+    this.name = name;
+    this.clicks = clicks;
+  }
+
+}
+
 let clicks = 0;
 let clickCounter = document.querySelector(".clicks");
 
-var btn = document.querySelector('.catPic');
+let btn = document.querySelector('img');
 btn.onclick = function clickCount() {
   clicks++;
   clickCounter.innerHTML = clicks;
   console.log(clicks);
 }
-class Cat {
-  constructor(name) {
-    this.name = name;
-  }
-}
-let catOne = new Cat("Goth Kitty");
+
+let theCatPic = document.querySelectorAll('.catPic');
+
+let catOne = new Cat("Goth Kitty",);
 let catTwo = new Cat("Paul Kitty");
-let catThree = new Cat("Sime Kitty");
+let catThree = new Cat("Sime Kitty",);
 let catFour = new Cat("Toodles Kitty");
 let catFive = new Cat("Farva Kitty");
+
+
+
+
+
+
+
+
+
 
